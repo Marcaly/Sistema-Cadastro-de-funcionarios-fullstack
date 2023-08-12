4bde0b373408d6f2db1b6c3a1316739ff0e63d7c
Marçal <byelmarcal@hotmail.com>
2023-08-12 16:29:50 -0300

    Projeto

---
+++ backend/src/main/java/com/marcal/crudbackend/domain/model/Employee.java
@@ -0,0 +1,26 @@
+package com.marcal.crudbackend.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "emailId")
+    private String emailId;
+}
