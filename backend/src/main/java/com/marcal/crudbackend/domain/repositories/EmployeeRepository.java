e28d018c5b120792af2c74f43d094ebc772a5168
Marçal <byelmarcal@hotmail.com>
2023-08-13 16:47:07 -0300

    rm r force

--- backend/src/main/java/com/marcal/crudbackend/domain/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marcal.crudbackend.domain.repositories;
-
-import com.marcal.crudbackend.domain.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
